578b4d74710358f42e0e9ca7092afadf5f440609
ivilthejoker-create <ivilthejoker@gmail.com>
2026-04-19 19:21:30 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// 1. كائن إدارة النقاط (Score Object)
+// 1. إدارة النقاط كأوبجكت (Score Object)
 let ScoreManager = {
     current: 0,
     high: localStorage.getItem('high_score') || 0,
@@ -25,31 +25,35 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-    // العودة للروابط المباشرة لضمان عدم ظهور "التيكستر"
+    // استخدام روابط مباشرة (لا تحتاج لتحميل صور على جهازك)
     this.load.image('background', 'https://labs.phaser.io/assets/skies/space3.png');    
     this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-ship.png');     
     this.load.image('enemy', 'https://labs.phaser.io/assets/sprites/slime.png');       
 }
 
 function create() {
-    // وضع الخلفية أولاً لضمان ظهور النص فوقها
+    // إضافة الخلفية أولاً لضمان ظهور كل شيء فوقها
     this.add.image(400, 300, 'background');
 
-    // كائن النص لعرض السكور
+    // إنشاء كائن النص لعرض النقاط (scoreText Object)
     scoreText = this.add.text(16, 16, 'النقاط: 0', { 
-        fontSize: '32px', fill: '#ffffff', fontStyle: 'bold' 
+        fontSize: '32px', 
+        fill: '#ffffff', 
+        fontStyle: 'bold' 
     });
 
+    // إنشاء اللاعب وتفعيل الحدود
     player = this.physics.add.sprite(400, 300, 'player');
     player.setCollideWorldBounds(true);
     
+    // تفعيل ميزة البريميوم (تغيير اللون للتميز)
     if (isPremium) {
-        player.setTint(0x00ff00); // ميزة البريميوم (تغيير اللون)
+        player.setTint(0x00ff00); 
     }
 
     enemies = this.physics.add.group();
     
-    // توليد عدو كل ثانية
+    // توليد عدو عشوائي كل ثانية
     this.time.addEvent({
         delay: 1000,
         callback: spawnEnemy,
@@ -59,9 +63,9 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    // منطق التصادم
+    // منطق التصادم والخسارة
     this.physics.add.overlap(player, enemies, () => {
-        alert("انتهت اللعبة! نقاطك: " + Math.floor(ScoreManager.current / 10));
+        alert("انتهت اللعبة! نقاطك الحالية: " + Math.floor(ScoreManager.current / 10));
         ScoreManager.reset();
         this.scene.restart();
     });
@@ -70,10 +74,11 @@ function create() {
 function spawnEnemy() {
     let x = Phaser.Math.Between(50, 750);
     let enemy = enemies.create(x, -50, 'enemy');
-    enemy.setVelocityY(200);
+    enemy.setVelocityY(200); // سرعة سقوط العدو لأسفل
 }
 
 function update() {
+    // تحديد السرعة بناءً على حالة التميز
     let speed = isPremium ? 550 : 300;
 
     if (cursors.left.isDown) player.setVelocityX(-speed);
@@ -84,14 +89,14 @@ function update() {
     else if (cursors.down.isDown) player.setVelocityY(speed);
     else player.setVelocityY(0);
 
-    // تحديث النقاط كأوبجكت
+    // تحديث النقاط كأوبجكت وعرضها
     ScoreManager.add(1);
     scoreText.setText('النقاط: ' + Math.floor(ScoreManager.current / 10));
 
-    // هدف الفوز عند 1000 نقطة
+    // هدف الفوز: الوصول لـ 1000 نقطة
     if (Math.floor(ScoreManager.current / 10) >= 1000) {
         this.physics.pause();
-        alert("مبروك! لقد حققت هدف الـ 1000 نقطة وفزت!");
+        alert("تهانينا! حققت هدف الـ 1000 نقطة وفزت باللعبة!");
         ScoreManager.reset();
         this.scene.restart();
     }
